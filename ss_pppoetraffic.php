5d6daa3e49f30aad375fc9acc1b38d0f819ba790
AlperBas <alper.bassarac@gmail.com>
2016-05-11 11:48:51 +0300

    update rrd logging

--- ss_pppoetraffic.php
+++ ss_pppoetraffic.php
@@ -354,7 +354,7 @@ function ss_pppoetraffic_GETOLDCOUNTERS($username) {
         @list($time, $counters['in'], $counters['out']) = @explode(" ", $oldcounters[2]);
     }
 
-    ss_pppoetraffic_LOGGER('file', "$username, rrd is $rrd");
+    ss_pppoetraffic_LOGGER('file', "Get old counters for $username, rrd is $rrd");
     return $counters;
 }
 
